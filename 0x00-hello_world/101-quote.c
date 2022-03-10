5f055cbbb8dc7b059c4c117e3c8c9f7496188128
Leealo <leeroykyalo@gmail.com>
2022-03-10 15:59:08 -0800

    added file 101-quote.c, script will print a string, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * This program prints a sentence and a date, followed by a new line, to the standard error
+ * This program prints a sentence and a date, followed
+ * by a new line, to the standard error
  * Return: 1 for success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, 59);
 	return (1);
 }
+
