347e13b480a0463722517435e9bc8c78e217084a
Leealo <leeroykyalo@gmail.com>
2022-03-10 15:04:23 -0800

    added file 4-puts.c, script will print a string followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * The program prints the string
+ * "Programming is like bulding a multilingual puzzle"
+ * followed by a new line
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programmin is like building a multilingual puzzle\n");
+
+	return (0);
+}
