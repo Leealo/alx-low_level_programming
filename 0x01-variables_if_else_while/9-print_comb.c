fa04d5ef87fb4f5c092dc3d80396f972ffc64ad9
Leealo <leeroykyalo@gmail.com>
2022-03-12 04:46:16 -0800

    added file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program prints possible combinations of
+ * single digit numbers separated by commas 
+ * in ascending order and are printed with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
