f734b3397276a8098d68c8dd511f777485294725
Leealo <leeroykyalo@gmail.com>
2022-03-15 18:24:23 -0700

    added file 10-add.c, the function adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: the first operand
+ * @b: the second operand
+ * Return: sum of the operands
+ */
+int add (int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
