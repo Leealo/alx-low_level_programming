48b63f2f12a99841656d21c4905d40f20cdeacaa
Leealo <leeroykyalo@gmail.com>
2022-03-15 19:11:28 -0700

    added file 4-isalpha.c, function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,21 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _isalpha - checks for an alphabet
+ * _isalpha - checks if input is a letter
+ *
  * @c: character to check
  *
- * Return: 1 if c is an alphabet, otherwise return 0
+ * Return: 1 for letters. Otherwise return 0.
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-			return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
 	else
-			return (0);
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
