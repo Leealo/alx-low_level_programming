772e629b66b7fa28f371e8a0e6e6140050387d38
Leealo <leeroykyalo@gmail.com>
2022-03-17 23:18:43 +0000

    added file 3-print_numbers.c, program prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 and 9
+ * Return: nil
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+}
