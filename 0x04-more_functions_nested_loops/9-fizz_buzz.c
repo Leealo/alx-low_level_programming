6d0cb352ba86818912fdf7de45008c1077fc339a
Leealo <leeroykyalo@gmail.com>
2022-03-18 00:51:23 +0000

    fixing errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,17 +2,17 @@
 
 /**
  * main - prints FizzBuzz for multiples of 3 and 5
- * prints Fizz for multiples of 3 only
- * prints Buzz for multiples of 5
+ * print Fizz for multiples of 3 only
+ * print Buzz for multiples of 5
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; n <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (n % 15 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
