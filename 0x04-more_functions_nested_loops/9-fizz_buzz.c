0159dd398647c3cf87298c59e30035b190935282
Leealo <leeroykyalo@gmail.com>
2022-03-18 00:42:39 +0000

    fixing errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,19 @@ int main(void)
 	for (i = 1; n <= 100; i++)
 	{
 		if (n % 15 == 0)
-		
 			printf("FizzBuzz");
 		
 		else if (i % 3 == 0)
-		
 			printf("Fizz");
-		
+
 		else if (i % 5 == 0)
-		
 			printf("Buzz");
 		
 		else
-		
 			printf("%i", i);
-		
+
 		if (i != 100)
-		
 			printf(" ");
-		
 	}
 	printf("\n");
 	return (0);
