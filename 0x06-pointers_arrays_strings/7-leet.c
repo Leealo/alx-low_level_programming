307ff546639d834ecf268c8bfe0973cbb0fec629
Leealo <leeroykyalo@gmail.com>
2022-03-24 23:04:19 +0000

    added file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,16 @@
  */
 char *leet(char *s)
 {
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int uup_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int i, j;
+	char leet [] = "oOlL22eEaA5566tT";
 
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-			{
-				*(s + count) = numbers[i];
-				break;
-			}
-		}
-		count++;
+		for (j = 0; j < 16; j++)
+
+			if (s[i] == leet[j])
+				s[i] = j / 2 + '0';
 	}
+
 	return (s);
 }
